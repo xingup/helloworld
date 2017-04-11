436996f1b42632b42e955bcea06aac35780eb206
xingup <1451519561@qq.com>
2017-04-11 10:19:31 +0800

    up

---
+++ javademo/src/Demo.java
@@ -0,0 +1,17 @@
+/**
+ * Created by RYX on 2017/4/11.
+ */
+// 中文是否乱码的是
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("this is a demo");
+        athis();
+    }
+
+    public static void athis() {
+
+    }
+}///////////////////////////////////////////////////////////////////////////
+// // STOPSHIP: 2017/4/11
+///////////////////////////////////////////////////////////////////////////
+
---
+++ javademo/src/Demo2.java
@@ -0,0 +1,5 @@
+/**
+ * Created by RYX on 2017/4/11.
+ */
+public class Demo2 {
+}
